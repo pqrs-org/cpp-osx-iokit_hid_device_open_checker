2cacec6ce23a675675e7228757decee5b99fdb72
Takayama Fumihiko <tekezo@pqrs.org>
2020-04-16 23:00:34 +0900

    update example

--- example/main.cpp
+++ example/main.cpp
@@ -15,8 +15,8 @@ int main(void) {
 
   std::vector<pqrs::cf::cf_ptr<CFDictionaryRef>> matching_dictionaries{
       pqrs::osx::iokit_hid_manager::make_matching_dictionary(
-          pqrs::osx::iokit_hid_usage_page::generic_desktop,
-          pqrs::osx::iokit_hid_usage::generic_desktop::keyboard),
+          pqrs::hid::usage_page::generic_desktop,
+          pqrs::hid::usage::generic_desktop::keyboard),
   };
 
   auto checker = std::make_unique<pqrs::osx::iokit_hid_device_open_checker>(dispatcher,
