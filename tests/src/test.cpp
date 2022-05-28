b697c46d15c614a5ba058bf704471b05dbb59091
Takayama Fumihiko <tekezo@pqrs.org>
2022-05-28 21:22:10 +0900

    catchorg/Catch2 -> boost-ext/ut

--- tests/src/test.cpp
+++ tests/src/test.cpp
@@ -1,25 +1,30 @@
-#define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
-
+#include <boost/ut.hpp>
 #include <pqrs/osx/iokit_hid_device_open_checker.hpp>
 
-TEST_CASE("iokit_hid_device_open_checker") {
-  auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
-  auto dispatcher = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
+int main(void) {
+  using namespace boost::ut;
+  using namespace boost::ut::literals;
 
-  std::vector<pqrs::cf::cf_ptr<CFDictionaryRef>> matching_dictionaries{
-      pqrs::osx::iokit_hid_manager::make_matching_dictionary(
-          pqrs::hid::usage_page::generic_desktop,
-          pqrs::hid::usage::generic_desktop::keyboard),
+  "iokit_hid_device_open_checker"_test = [] {
+    auto time_source = std::make_shared<pqrs::dispatcher::hardware_time_source>();
+    auto dispatcher = std::make_shared<pqrs::dispatcher::dispatcher>(time_source);
+
+    std::vector<pqrs::cf::cf_ptr<CFDictionaryRef>> matching_dictionaries{
+        pqrs::osx::iokit_hid_manager::make_matching_dictionary(
+            pqrs::hid::usage_page::generic_desktop,
+            pqrs::hid::usage::generic_desktop::keyboard),
+    };
+
+    auto checker = std::make_unique<pqrs::osx::iokit_hid_device_open_checker>(dispatcher,
+                                                                              matching_dictionaries);
+
+    checker->async_start();
+
+    checker = nullptr;
+
+    dispatcher->terminate();
+    dispatcher = nullptr;
   };
 
-  auto checker = std::make_unique<pqrs::osx::iokit_hid_device_open_checker>(dispatcher,
-                                                                            matching_dictionaries);
-
-  checker->async_start();
-
-  checker = nullptr;
-
-  dispatcher->terminate();
-  dispatcher = nullptr;
+  return 0;
 }
